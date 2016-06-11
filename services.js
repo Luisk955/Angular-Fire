386f933d3d298835ec84964f85a7ca412124835b
Luisk Del Valle <Luisk Del Valle>
2016-06-10 20:56:29 -0600

    nobower(?)

--- services.js
+++ services.js
@@ -1,1 +1,9 @@
-angular.module('videoGames.services')
+angular.module('videoGames.services',[])
+
+.service('fbRef', function(){
+	return new Firebase(fbURL);
+})
+
+.service('Games', function($firebase, fbRef, gameListValue){
+
+});
